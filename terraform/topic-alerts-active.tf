c35c4baa4518d93225c8d96e7f29c68d43d8a26f
Sandon Jacobs <sjacobs@confluent.io>
2024-02-13 15:28:46 -0500

    Renamed files, connectors working with avro formatted data.

--- terraform/topic-alerts-active.tf
+++ terraform/topic-alerts-active.tf
@@ -1,3 +1,29 @@
+# inbound active alerts
+resource "confluent_kafka_topic" "noaa_alerts_active_inbound" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name       = "NoaaAlertsActiveInbound"
+  rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+
+  partitions_count = 3
+  config = {
+    "cleanup.policy" = "compact"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  depends_on = [
+    confluent_schema_registry_cluster.essentials
+  ]
+}
+
 resource "confluent_schema" "noaa_alerts_active_inbound_value" {
 
   schema_registry_cluster {
@@ -13,18 +39,15 @@ resource "confluent_schema" "noaa_alerts_active_inbound_value" {
   format       = "AVRO"
   subject_name = "${confluent_kafka_topic.noaa_alerts_active_inbound.topic_name}-value"
 
-  schema = file("../data/src/main/avro/NoaaAlertsActive-value.avsc")
+  schema = file("../data/src/main/avro/NoaaAlertsActiveInbound-value.avsc")
 
   lifecycle {
     prevent_destroy = false
   }
 
-#  depends_on = [
-#    confluent_schema_registry_cluster.essentials,
-#    confluent_api_key.env-manager-schema-registry-api-key,
-#    confluent_role_binding.sr_resource_owner_rb,
-#    confluent_kafka_topic.noaa_alerts_active_inbound
-#  ]
+  depends_on = [
+    confluent_kafka_topic.noaa_alerts_active_inbound
+  ]
 }
 
 resource "confluent_subject_config" "noaa_alerts_active_inbound_value_cfg" {
@@ -48,3 +71,4 @@ resource "confluent_subject_config" "noaa_alerts_active_inbound_value_cfg" {
     confluent_schema.noaa_alerts_active_inbound_value
   ]
 }
+
---
+++ terraform/topic-zones.tf
@@ -0,0 +1,73 @@
+#inbound zone definitions
+resource "confluent_kafka_topic" "noaa_zones_inbound" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  topic_name    = "NoaaZonesInbound"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+
+  partitions_count = 1
+  config           = {
+    "cleanup.policy" = "compact"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  depends_on = [
+    confluent_schema_registry_cluster.essentials
+  ]
+}
+
+resource "confluent_schema" "noaa_zones_inbound_value" {
+
+  schema_registry_cluster {
+    id = confluent_schema_registry_cluster.essentials.id
+  }
+  rest_endpoint = confluent_schema_registry_cluster.essentials.rest_endpoint
+
+  credentials {
+    key    = confluent_api_key.env-manager-schema-registry-api-key.id
+    secret = confluent_api_key.env-manager-schema-registry-api-key.secret
+  }
+
+  format       = "AVRO"
+  subject_name = "${confluent_kafka_topic.noaa_zones_inbound.topic_name}-value"
+
+  schema = file("../data/src/main/avro/NoaaZonesInbound-value.avsc")
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  depends_on = [
+    confluent_kafka_topic.noaa_zones_inbound
+  ]
+}
+
+resource "confluent_subject_config" "noaa_zones_inbound_value_cfg" {
+  schema_registry_cluster {
+    id = confluent_schema_registry_cluster.essentials.id
+  }
+  rest_endpoint       = confluent_schema_registry_cluster.essentials.rest_endpoint
+  subject_name        = "${confluent_kafka_topic.noaa_zones_inbound.topic_name}-value"
+  compatibility_level = "NONE"
+
+  credentials {
+    key    = confluent_api_key.env-manager-schema-registry-api-key.id
+    secret = confluent_api_key.env-manager-schema-registry-api-key.secret
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  depends_on = [
+    confluent_schema.noaa_zones_inbound_value
+  ]
+}
