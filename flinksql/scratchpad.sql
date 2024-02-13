c331e67c577f8cd159e2f2476b8a2cf0220fb5bf
Sandon Jacobs <sjacobs@confluent.io>
2024-02-13 15:29:59 -0500

    flink scratchpad thoughts.

--- flinksql/scratchpad.sql
+++ flinksql/scratchpad.sql
@@ -14,25 +14,32 @@ select
 from noaa_alerts_active_inbound limit 10;
 
 
-create table foo (
+create table foo_step_2 (
     id STRING PRIMARY KEY NOT ENFORCED,
     event_url STRING,
-    geoCodesSAME ARRAY<STRING>
+    geoCodesUGC STRING
 ) with ('value.format' = 'avro-registry');
 
 
-insert into foo select
+insert into foo_step_2 select
                     JSON_VALUE(DECODE(val, 'UTF-8'), '$.properties.id') as id,
                     JSON_VALUE(DECODE(val, 'UTF-8'), '$.id') as event_url,
-                    JSON_QUERY(DECODE(val, 'UTF-8'), '$.properties.geocode.SAME') as geoCodesSAME
+                    JSON_QUERY(DECODE(val, 'UTF-8'), '$.properties.geocode.UGC' WITH CONDITIONAL ARRAY WRAPPER) as geoCodesUGC
                 from noaa_alerts_active_inbound;
 
 
+select id, event_url, geo_code_ugc from foo_step_2 cross join unnest(JSON_STRING(geoCodesUGC)) as g (geo_code_ugc) limit 100;
+
+
+create table alert_rowed (
+    id STRING PRIMARY KEY NOT ENFORCED,
+
+)
 
 select
     JSON_VALUE(DECODE(val, 'UTF-8'), '$.id') as event_url,
     JSON_VALUE(DECODE(val, 'UTF-8'), '$.properties.id') as id,
-    CAST(JSON_QUERY(DECODE(val, 'UTF-8'), '$.properties.geocode.SAME') as STRING) as geoCodesSAME,
+    CAST(JSON_QUERY(DECODE(val, 'UTF-8'), '$.properties.geocode.UGC') as STRING) as geoCodesUGC,
     JSON_QUERY(DECODE(val, 'UTF-8'), '$.properties.affectedZones') as affectedZones,
     JSON_VALUE(DECODE(val, 'UTF-8'), '$.properties.sent') as sentTs,
     JSON_VALUE(DECODE(val, 'UTF-8'), '$.properties.effective') as effectiveTs,
