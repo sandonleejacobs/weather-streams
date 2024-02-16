5dd2cf7d61fc8bb6b14b622ec52580941ab5c125
Sandon Jacobs <sjacobs@confluent.io>
2024-02-16 11:37:45 -0500

    new table for active alerts by zone

---
+++ flinksql/src/main/sql/create-tables/WsActiveAlertsByUgcCode.sql
@@ -0,0 +1,31 @@
+create table WsActiveAlertsByZone (
+    alertId STRING,
+    zoneId STRING,
+    zoneUrl STRING,
+    event STRING,
+    alertStatus STRING,
+    alertMessageType STRING,
+    zoneName STRING,
+    zoneState STRING,
+    category STRING,
+    severity STRING,
+    urgency STRING,
+    headline STRING,
+    description STRING,
+    instruction STRING,
+    response STRING,
+    zoneGeometry STRING,
+    sentTs TIMESTAMP_LTZ(3),
+    sender STRING,
+    onsetTs TIMESTAMP_LTZ(3),
+    effectiveTs TIMESTAMP_LTZ(3),
+    expiryTs TIMESTAMP_LTZ(3),
+    endTs TIMESTAMP_LTZ(3),
+    nwsHeadline ARRAY<STRING>,
+    PRIMARY KEY (alertId, zoneId) NOT ENFORCED
+)
+with (
+    'key.format' = 'avro-registry',
+    'value.format' = 'avro-registry',
+    'changelog.mode' = 'upsert',
+    'kafka.cleanup-policy' = 'compact');
---
+++ flinksql/src/main/sql/insert-into/WsActiveAlertsByZone.sql
@@ -0,0 +1,28 @@
+insert into WsActiveAlertsByZone select
+    active.`id` as `alertId`,
+    `ActiveAlertsByUgcCode`.geocodeugc as `zoneId`,
+    zone.`url` as `zoneUrl`,
+    active.`event` as `event`,
+    active.`status` as `alertStatus`,
+    active.`messageType` as `alertMessageType`,
+    zone.`name` as `zoneName`,
+    zone.`state` as `zoneState`,
+    active.`category` as `category`,
+    active.`severity` as `severity`,
+    active.`urgency` as `urgency`,
+    active.`headline` as `headline`,
+    active.`description` as `description`,
+    active.`instruction` as `instruction`,
+    active.`response` as `response`,
+    zone.`geometry` as `zoneGeometry`,
+    active.`sent` as `sentTs`,
+    active.`sender` as `sender`,
+    active.`onset` as `onsetTs`,
+    active.`effective` as `effectiveTs`,
+    active.`expiry` as `expiryTs`,
+    active.`end` as `endTs`,
+    active.`NWSHeadline` as `nwsHeadline`
+from ws_active_alerts active
+    CROSS JOIN UNNEST(active.geocodeUGC) as `ActiveAlertsByUgcCode`(geocodeugc)
+join NoaaZonesInbound zone
+    on `ActiveAlertsByUgcCode`.geocodeugc = zone.id;
